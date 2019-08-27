f5e8eca863ae8e37084ca0bceec6205158bf30c0
khaazz <khaaz.dev@gmail.com>
2019-08-27 16:23:08 +0200

    WIP - basic idea behind docgen

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-/* options = 
+const Generator = require('./Generator');
+
+/* options =
 [
     {
         "input": "path",
@@ -8,5 +10,10 @@
 */
 
 function main(options) {
+    const generator = new Generator();
+    for (const elem of options) {
+        generator.render(elem.input);
+        // write resulte in correct file elem.output
+    }
 // run doc gen for every input / output
 }
